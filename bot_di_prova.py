17203bb260638fe4e2e98424e8b32712483a9481
Tommaso Moro <42266178+TommasoMoro03@users.noreply.github.com>
2023-12-20 15:48:05 +0100

    Bot di prova OpenAI

--- bot_di_prova.py
+++ bot_di_prova.py
@@ -2,16 +2,15 @@ import telebot
 import openai
 from openai import OpenAI
 
-bot = telebot.TeleBot ('6270677232:AAGA_Yi-cZ523XUT63n3B3pCwP7CKOHstnM')
+bot = telebot.TeleBot ('MY_TELEGRAM_API_KEY')
 
 client = OpenAI(
-    # defaults to os.environ.get("OPENAI_API_KEY")
-    api_key="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",
+    api_key="MY_OPENAI_API_KEY",
 )
 
 @bot.message_handler(commands=['start'])
 def main(message):
-     msg = bot.send_message(message.chat.id, "Go on and ask anything")
+     msg = bot.send_message(message.chat.id, "Chiedi qualcosa")
      bot.register_next_step_handler(msg, chatgpt)
 
 @bot.message_handler(content_types=['text'])
